08c75ed442af0a0c37bf3845553504f7265fb842
devDaniel28 <113447778+devdaniel28@users.noreply.github.com>
2025-11-21 15:33:18 -0300

    Estilo é reponsividade pronta

---
+++ src/script/script.js
@@ -0,0 +1,10 @@
+function ConverterIp() {
+    const resultBinary = document.querySelector('#resultbinary')
+    const textIP = document.querySelector('#areaip').value
+    const binaryConversionTable = [ 1, 2, 4, 8, 16, 32, 64, 128 ]
+    const numberIp = textIP.split('')  
+
+    if (numberIp) {
+        
+    }
+}
