00ba086c333ce9dbb3ad7e341aa41f7aa5f96405
devDaniel28 <113447778+devdaniel28@users.noreply.github.com>
2025-12-01 16:14:08 -0300

    Ainda sem chegar em um resultado pra esse site

--- src/script/script.js
+++ src/script/script.js
@@ -1,10 +1,6 @@
 function ConverterIp() {
     const resultBinary = document.querySelector('#resultbinary')
     const textIP = document.querySelector('#areaip').value
-    const binaryConversionTable = [ 1, 2, 4, 8, 16, 32, 64, 128 ]
-    const numberIp = textIP.split('')  
-
-    if (numberIp) {
-        
-    }
+    const binaryConversionTable = [128, 64, 32, 16, 8, 4, 2]
+    const numberIp = textIP.split('.')  
 }
