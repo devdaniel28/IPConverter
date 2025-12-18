5ff46689a90649f9e03a95973493f0626aa813d9
devDaniel28 <113447778+devdaniel28@users.noreply.github.com>
2025-12-18 18:23:45 -0300

    Finalmente descobrir como criar isso, e agora tenho meus agradecimentos ao livro Entendendo Algoritmos de Aditya Y. Bhargava

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,29 @@
-function ConverterIp() {
-    const resultBinary = document.querySelector('#resultbinary')
+function conversionToBinary(ip) {
+    const octetos = ip.split('.')
+     if(octetos.length  !== 4) {
+        throw new Error('Ip invalido: deve-se ter 4 octetos')
+     }
+    
+    const result = []
+     for (let x = 0; x < 4; x++) {
+        const octeto = parseInt(octetos[x], 10)
+
+        if(isNaN(octeto) || octeto < 0 || octeto > 255) {
+            throw new Error(`Octeto Invalido: ${octetos[i]}`)
+        }
+
+        let binary = octeto.toString(2)
+        binary = '0'.repeat(8 - binary.length) + binary
+        result.push(binary)
+     }
+
+     return result.join('.')
+}
+
+function printResult() {
     const textIP = document.querySelector('#areaip').value
-    const binaryConversionTable = [128, 64, 32, 16, 8, 4, 2]
-    const numberIp = textIP.split('.')  
+    const resultBinaryDOM = document.querySelector('#resultbinary')
+    const binaryProcess = conversionToBinary(textIP)
+
+    return resultBinaryDOM.value = binaryProcess
 }
